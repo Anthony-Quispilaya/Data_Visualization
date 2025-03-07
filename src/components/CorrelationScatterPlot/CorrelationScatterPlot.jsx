e7262ea157f1b246642ee1e914e7d4b2b9bd1754
root <root@AnthonyASUS>
2025-03-07 01:03:17 -0500

    Initial skeleton of CorrelationScatterPlot component

---
+++ src/components/CorrelationScatterPlot/CorrelationScatterPlot.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './CorrelationScatterPlot.css';
+
+const CorrelationScatterPlot = () => {
+  return (
+    <div className="correlation-scatter-plot-container">
+      <h2>Correlation Analysis: Air Quality vs. Respiratory Health</h2>
+      <div className="chart-container">
+        <p>Correlation scatter plot component is under development.</p>
+      </div>
+    </div>
+  );
+};
+
+export default CorrelationScatterPlot;
