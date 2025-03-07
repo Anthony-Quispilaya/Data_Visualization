397180f8629b7412b2f21aae629ba35c3fd49f99
root <root@AnthonyASUS>
2025-03-06 23:04:01 -0500

    Add EnhancedUSMap component to App.js

--- src/App.js
+++ src/App.js
@@ -2,10 +2,15 @@ import React from 'react';
 /* import RespiratoryChart from './RespiratoryChart/RespiratoryChart'; */
 import InfluenzaChart from './InfluenzaChart/InfluenzaChart';
 import AirQualityDashboard from './AirQuality/AirQualityDashboard';
+import EnhancedUSMap from './components/USMap/EnhancedUSMap';
 
 function App() {
   return (
     <div className="App">
+      <h1 style={{ textAlign: 'center', marginBottom: '30px' }}>
+        Health and Environmental Data Visualization
+      </h1>
+      <EnhancedUSMap />
       <InfluenzaChart />
       <AirQualityDashboard />
     </div>
